865cfed819ef6b7c93e2fb4b566bc3ad0d212576
KafuuKion <34386894+KafuuKion@users.noreply.github.com>
2021-07-23 17:31:51 +0800

    Create Filter-by-author-and-unlike.js

---
+++ Filter-by-author-and-unlike.js
@@ -0,0 +1,18 @@
+// Remember to scroll down all the way to the end so that all entries in the current playlist are loaded!
+// Paste the following into the console and press Enter/Return(⏎) to execute the code.
+// Depending on your hardware, the browser or the page may lose response for a few seconds or even minutes.
+// Stay patient before you can see the script takes effect.
+
+const author_name = REPLACE THIS WITH YOUR AUTHOR;
+var items = document.querySelectorAll(".ytmusic-responsive-list-item-renderer");
+for (var i = 0; i < items.length; i++) {
+    if (items[i].getAttribute("title") === author_name) {
+        var temp = items[i].parentNode.parentNode.parentNode.querySelectorAll(".like");
+        for (var j = 0; j < temp.length; j++) {
+            if (temp[j].getAttribute("aria-pressed") === "true") {
+                temp[j].click();
+                console.log('Unliked an entry');
+            }
+        }
+    }
+}
