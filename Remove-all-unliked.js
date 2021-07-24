6d19937ad3c52c53ed9c7ef47ae83c2ebaa8c589
KafuuKion <34386894+KafuuKion@users.noreply.github.com>
2021-07-24 12:50:45 +0800

    Update Remove-all-unliked.js

--- Remove-all-unliked.js
+++ Remove-all-unliked.js
@@ -4,14 +4,31 @@
 // Stay patient before you can see the script takes effect.
 
 // This one doesn't work yet.
+// Credit: https://www.reddit.com/r/YoutubeMusic/comments/hko80z/how_to_clean_your_youtube_music_library_songs/
+
 
 var items = document.querySelectorAll(".like");
-for (var i = 0; i < items.length; i++) {
+for (var j = 0; j < items.length; j++) {
     if (items[i].getAttribute("aria-pressed") === "false") {
-        var temp = items[i].parentNode.parentNode.querySelectorAll(".dropdown-trigger");
-        temp[0].click();
-        var menu = temp[j].querySelectorAll(".ytmusic-menu-service-item-renderer");
-        menu[2].click();
-        console.log('Removed one from playlist.');
+        var song = items[i].parentNode.parentNode.querySelectorAll(".dropdown-trigger.ytmusic-menu-renderer");
+        for(var i = 0; i < song.length; i++) {
+            song[i].click(); 
+            var dropdown = document.body.querySelector("ytmusic-menu-popup-renderer[slot='dropdown-content']");
+            if(dropdown != undefined) {
+                var remove = dropdown.querySelector("paper-listbox#items").querySelector("ytmusic-toggle-menu-service-item-renderer.ytmusic-menu-popup-renderer");
+                if(remove != null) {
+                    actualRemove = remove.querySelector('yt-formatted-string.ytmusic-toggle-menu-service-item-renderer');
+                    if(actualRemove != null) {
+                        if(actualRemove.innerHTML == 'プレイリストから削除') {
+                            remove.click();
+                            await new Promise(r => setTimeout(r, 1000));
+                            console.log("プレイリストから削除");
+                        }
+                    }
+                }
+            }
+        }
+        await new Promise(r => setTimeout(r, 1000));
     }
 }
+
