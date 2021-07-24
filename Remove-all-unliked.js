13c213e6ca52ef624a9db8c11bd3d83bd19b855a
KafuuKion <34386894+KafuuKion@users.noreply.github.com>
2021-07-24 13:57:51 +0800

    Update Remove-all-unliked.js

--- Remove-all-unliked.js
+++ Remove-all-unliked.js
@@ -4,31 +4,27 @@
 // Stay patient before you can see the script takes effect.
 
 // This one doesn't work yet.
+// The dropdown menu just refuses to appear.
 // Credit: https://www.reddit.com/r/YoutubeMusic/comments/hko80z/how_to_clean_your_youtube_music_library_songs/
 
 
 var items = document.querySelectorAll(".like");
 for (var j = 0; j < items.length; j++) {
-    if (items[i].getAttribute("aria-pressed") === "false") {
-        var song = items[i].parentNode.parentNode.querySelectorAll(".dropdown-trigger.ytmusic-menu-renderer");
-        for(var i = 0; i < song.length; i++) {
-            song[i].click(); 
-            var dropdown = document.body.querySelector("ytmusic-menu-popup-renderer[slot='dropdown-content']");
-            if(dropdown != undefined) {
-                var remove = dropdown.querySelector("paper-listbox#items").querySelector("ytmusic-toggle-menu-service-item-renderer.ytmusic-menu-popup-renderer");
-                if(remove != null) {
-                    actualRemove = remove.querySelector('yt-formatted-string.ytmusic-toggle-menu-service-item-renderer');
-                    if(actualRemove != null) {
-                        if(actualRemove.innerHTML == 'プレイリストから削除') {
-                            remove.click();
-                            await new Promise(r => setTimeout(r, 1000));
-                            console.log("プレイリストから削除");
-                        }
-                    }
+    if (items[j].getAttribute("aria-pressed") === "false") {
+        var song = items[j].parentNode.parentNode.parentNode.querySelectorAll(".dropdown-trigger"); 
+        for(var i = 0; i < song.length ; i++) {
+            console.log('found')
+            song[i].scrollIntoView(); 
+            song[i].click();
+            setTimeout(alert(),1000);
+            var dropdown = document.body.querySelector("ytmusic-menu-popup-renderer.scroller");
+            var remove = dropdown.querySelectorAll("yt-formatted-string");
+            for(var k = 0; k < remove.length ; k++) {
+                if(remove.innerHTML == 'プレイリストから削除') {
+                    remove.click();
+                    console.log("removed from library");
                 }
             }
         }
-        await new Promise(r => setTimeout(r, 1000));
     }
 }
-
